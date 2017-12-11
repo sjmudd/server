a6007e3ca4615dc01069dcdc18eafebd9e1505ca
Vesa Pentti <vesa.pentti@mariadb.net>
2017-12-11 19:20:37 +0000

    MDEV-14315 -- Reflect use of tcmalloc in a system variable and error log
      * The version of tcmalloc is written to the system variable
        'version_malloc_library' if tcmalloc is used, similarly to
        jemalloc
      * Extracted method guess_malloc_library()

---
+++ sql/guess_malloc_library.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2002, 2015, Oracle and/or its affiliates.
+   Copyright (c) 2012, 2017, MariaDB Corporation.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
+
+#ifndef GUESS_MALLOC_LIBRARY_INCLUDED
+#define GUESS_MALLOC_LIBRARY_INCLUDED
+
+char *guess_malloc_library();
+
+#endif /* GUESS_MALLOC_LIBRARY_INCLUDED */
+
